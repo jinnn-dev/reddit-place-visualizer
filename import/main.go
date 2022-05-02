75a86b2c9d4b257ca27f3d17130308ce78f4548a
Finn Beer <finn.beer@gmail.com>
2022-05-02 19:32:16 +0200

    added faster importer

---
+++ import/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type Entry struct {
+	UserId     string `json:"user_id"`
+	PixelColor string `json:"pixel_color"`
+	X          int    `json:"x"`
+	Y          int    `json:"y"`
+}
+
+func main() {
+
+	var ctx = context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	file, err := os.Open("../2022_place_canvas_history.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	parser := csv.NewReader(file)
+
+	parser.Read()
+
+	buffer := make([]*redis.Z, 0)
+
+	count := 0
+
+	last_insert := time.Now()
+
+	for {
+		record, err := parser.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		count++
+
+		timestamp, _ := time.Parse("2006-02-01 15:04:05 UTC", record[0])
+
+		coords := strings.Split(record[3], ",")[:2]
+
+		x, _ := strconv.Atoi(coords[0])
+
+		y, _ := strconv.Atoi(coords[1])
+
+		entry := &Entry{
+			UserId:     record[1],
+			PixelColor: record[2],
+			X:          x,
+			Y:          y,
+		}
+
+		b, err := json.Marshal(entry)
+
+		buffer = append(buffer, &redis.Z{
+			Score:  float64(timestamp.UTC().UnixNano() / 1000000),
+			Member: string(b),
+		})
+
+		if count%1000000 == 0 {
+			fmt.Printf("%d million rows processed in %d milliseconds \n", count/1000000, time.Since(last_insert)/time.Millisecond)
+			rdb.ZAdd(ctx, "newpixels", buffer...)
+			buffer = make([]*redis.Z, 0)
+
+			last_insert = time.Now()
+		}
+
+	}
+}
---
+++ import/readme.md
@@ -0,0 +1,11 @@
+# Importer
+
+```go
+// install packages
+go get 
+```
+
+```go
+// run the programm (expects the csv in the main folder)
+go run main.go
+```
