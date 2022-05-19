bf1edb7772c83f0f925c5d8e0f1092d708c3fd83
noxz-dev <finn.beer@gmail.com>
2022-05-19 20:14:28 +0200

    moved click event

--- frontend/src/lib/redis.ts
+++
@@ -1,6 +0,0 @@
-
-export async function getDataBetweenTimestamps(start: number = 1, stop: number = 2) {
-    const result = await fetch(`http://localhost:5000/dataBetweenTimestamps?start=${start}&stop=${stop}`)
-
-    return await result.json();
-}
--- frontend/src/renderer/2d/canvasEvents.ts
+++ frontend/src/renderer/2d/canvasEvents.ts
@@ -1,5 +1,6 @@
 import type { CanvasRenderer } from '@/renderer/2d/canvasRenderer';
 import { registerSpacebarEvent } from '@/lib/events';
+import { HoverService } from '@/services/HoverService';
 
 export class CanvasEvents {
   lastX: number = 0;
@@ -7,6 +8,8 @@ export class CanvasEvents {
   dragStartX: number = 0;
   dragStartY: number = 0;
   isMouseDown: boolean = false;
+  startX: number = 0;
+  startY: number = 0;
 
   constructor(public renderer: CanvasRenderer) {}
 
@@ -30,6 +33,35 @@ export class CanvasEvents {
     this.isMouseDown = true;
   };
 
+  scale = (inputY: number, yRange: Array<number>, xRange: Array<number>): number => {
+    const [xMin, xMax] = xRange;
+    const [yMin, yMax] = yRange;
+
+    const percent = (inputY - yMin) / (yMax - yMin);
+    const outputX = percent * (xMax - xMin) + xMin;
+
+    return outputX;
+  };
+
+  getPixel = async (event: MouseEvent) => {
+    const rect = this.renderer.canvas.getBoundingClientRect();
+    const x = event.clientX;
+    const y = event.clientY;
+
+    const scaledX = Math.floor(this.scale(x, [rect.left, rect.right], [0, 2000]));
+    const scaledY = Math.floor(this.scale(y, [rect.top, rect.bottom], [0, 2000]));
+
+    try {
+      const position = { x: scaledX, y: scaledY };
+      console.log(position);
+
+      const data = await HoverService.getPixelData(position);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   panMouseMoveEvent = (event: MouseEvent) => {
     if (this.isMouseDown) {
       this.lastX = event.offsetX || event.pageX - this.renderer.canvas.offsetLeft;
@@ -44,6 +76,22 @@ export class CanvasEvents {
     this.renderer.canvas.parentElement!.addEventListener('mousedown', this.panMouseDownEvent);
 
     this.renderer.canvas.parentElement!.addEventListener('mousemove', this.panMouseMoveEvent);
+
+    const delta = 6;
+
+    this.renderer.canvas.addEventListener('mousedown', (event: MouseEvent) => {
+      this.startX = event.pageX;
+      this.startY = event.pageY;
+    });
+
+    this.renderer.canvas.addEventListener('mouseup', (event) => {
+      const diffX = Math.abs(event.pageX - this.startX);
+      const diffY = Math.abs(event.pageY - this.startY);
+
+      if (diffX < delta && diffY < delta) {
+        this.getPixel(event);
+      }
+    });
 
     this.renderer.canvas.parentElement!.addEventListener('mouseup', (event: MouseEvent) => {
       this.isMouseDown = false;
