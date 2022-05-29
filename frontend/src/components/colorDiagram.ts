6e0c787367cbd6e18025ab237b85a0152e6e6745
James Neumann <41788164+JamesNeumann@users.noreply.github.com>
2022-05-29 23:19:29 +0200

    Resizable Container for diagrams

--- frontend/src/components/colorDiagram.ts
+++ frontend/src/components/colorDiagram.ts
@@ -4,22 +4,50 @@ import type { ECBasicOption } from 'echarts/types/dist/shared';
 import { pixelColors } from '@/model/colorMapping';
 
 interface ChartItem {
-  name: string,
-  value: number,
+  name: string;
+  value: number;
 }
 
 export class ColorDiagram {
-
   chart: ECharts | undefined;
   options: ECBasicOption;
 
-  chartItems: ChartItem[];
+  chartItems!: ChartItem[];
 
-  palette: string[] = ['#6D001A', '#BE0039', '#FF4500', '#FFA800', '#FFD635', '#FFF8B8', '#00A368',
-    '#00CC78', '#7EED56', '#00756F', '#009EAA', '#00CCC0', '#2450A4', '#3690EA',
-    '#51E9F4', '#493AC1', '#6A5CFF', '#94B3FF', '#811E9F', '#B44AC0',
-    '#E4ABFF', '#DE107F', '#FF3881', '#FF99AA', '#6D482F', '#9C6926',
-    '#FFB470', '#000000', '#515252', '#898D90', '#D4D7D9', '#FFFFFF'];
+  palette: string[] = [
+    '#6D001A',
+    '#BE0039',
+    '#FF4500',
+    '#FFA800',
+    '#FFD635',
+    '#FFF8B8',
+    '#00A368',
+    '#00CC78',
+    '#7EED56',
+    '#00756F',
+    '#009EAA',
+    '#00CCC0',
+    '#2450A4',
+    '#3690EA',
+    '#51E9F4',
+    '#493AC1',
+    '#6A5CFF',
+    '#94B3FF',
+    '#811E9F',
+    '#B44AC0',
+    '#E4ABFF',
+    '#DE107F',
+    '#FF3881',
+    '#FF99AA',
+    '#6D482F',
+    '#9C6926',
+    '#FFB470',
+    '#000000',
+    '#515252',
+    '#898D90',
+    '#D4D7D9',
+    '#FFFFFF'
+  ];
 
   constructor(elementId: string) {
     const element = document.getElementById(elementId);
@@ -28,14 +56,14 @@ export class ColorDiagram {
     }
     this.options = {};
     this.initChart(element);
+    this.initChartItems();
 
-    this.chartItems = [];
+    const resizeCallback = () => {
+      this.chart!.resize();
+    };
 
-    for (let i = 0; i < pixelColors.length; i++) {
-      this.chartItems.push({
-        name: '' + i,
-        value: 0
-      });
+    if (element.parentElement !== null) {
+      new ResizeObserver(resizeCallback).observe(element.parentElement);
     }
   }
 
@@ -43,25 +71,32 @@ export class ColorDiagram {
     this.chart = init(element, 'dark', { renderer: 'svg' });
     this.options = {
       title: {
-        text: 'Referer of a Website',
-        subtext: 'Fake Data',
+        text: 'Color Distribution',
         left: 'center'
       },
+      backgroundColor: 'transparent',
       tooltip: {
         trigger: 'item'
       },
-      legend: {
-        orient: 'vertical',
-        left: 'left'
-      },
+
       series: [
         {
-          name: 'Access From',
           type: 'pie',
-          radius: '50%',
           data: this.chartItems,
           animate: false,
           color: this.palette,
+          radius: '85%',
+          center: ['50%', '52%'],
+          itemStyle: {
+            normal: {
+              label: {
+                show: false
+              },
+              labelLine: {
+                show: false
+              }
+            }
+          },
           emphasis: {
             itemStyle: {
               shadowBlur: 10,
@@ -70,14 +105,31 @@ export class ColorDiagram {
             }
           }
         }
-      ]
+      ],
+      animationDuration: 300,
+      animationDurationUpdate: 300
     };
     this.chart.setOption(this.options);
 
     setInterval(() => {
+      // @ts-ignore
       this.options.series[0].data = this.chartItems;
-      this.chart?.setOption(this.options);
-    }, 1000);
+      this.chart?.setOption({
+        series: {
+          data: this.chartItems
+        }
+      });
+    }, 300);
+  }
+
+  private initChartItems() {
+    this.chartItems = [];
+    for (let i = 0; i < this.palette.length; i++) {
+      this.chartItems.push({
+        name: this.palette[i],
+        value: 0
+      });
+    }
   }
 
   updateData(data: number[]) {
--- frontend/src/views/PlaceHistory.vue
+++ frontend/src/views/PlaceHistory.vue
@@ -1,10 +1,11 @@
 <script setup lang='ts'>
 import PlaceRender from '@/components/PlaceRenderer.vue';
+import ResizableContainer from '@/components/ResizableContainer.vue';
 
 </script>
 <template>
   <PlaceRender></PlaceRender>
+  <ResizableContainer><div id='color-diagram' style='width: 100%; height: 100%'></div></ResizableContainer>
   <div id='activity' style='width: 1000px;height:700px;float:right;background-color: black;'></div>
-  <div id='color-diagram'
-       style='width: 1000px; height: 700px; float: left; position:absolute; background-color: black'></div>
+
 </template>
